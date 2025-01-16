4589f8a5f07785674cf6016c241a2b4c26c8221b
Nick Jansen <n.jansen@sitepilot.io>
2025-01-16 12:15:06 +0100

    Remove Mailpit service and related configurations.
    The Mailpit service definition and its conditional inclusion in Docker Compose have been removed. This simplifies the configuration and eliminates the unused Mailpit service setup.

--- lib/environment.sh
+++ lib/environment.sh
@@ -17,12 +17,8 @@ flight_environment() {
   export FLIGHT_NETWORK=${FLIGHT_NETWORK:-flight}
   export FLIGHT_HTTP_PORT=${FLIGHT_HTTP_PORT:-80}
   export FLIGHT_HTTPS_PORT=${FLIGHT_HTTPS_PORT:-443}
-  export FLIGHT_MAILPIT_ENABLED=${FLIGHT_MAILPIT_ENABLED:-false}
   export FLIGHT_PHP_VERSION=${FLIGHT_PHP_VERSION:-"8.3"}
 
+  # shellcheck disable=SC2034
   DOCKER_COMPOSE="docker compose -f $FLIGHT_HOME_DIR/compose.yml"
-
-  if [ "${FLIGHT_MAILPIT_ENABLED}" == true ]; then
-    DOCKER_COMPOSE="$DOCKER_COMPOSE -f $FLIGHT_HOME_DIR/compose.mailpit.yml"
-  fi
 }
